f7da896e2c898e64e61f0d8f30f795cf97d8dc48
haiyutian25 <haiyutian25@users.noreply.github.com>
2026-08-26 03:40:58 +0800

    修复弃用警告：将createAndroidComposeRule迁移到junit4.v2 API（test-app/AppTest.kt、feature-mymodel/MyModelScreenTest.kt）

--- test-app/src/main/java/android/template/test/app/AppTest.kt
+++ test-app/src/main/java/android/template/test/app/AppTest.kt
@@ -1,6 +1,6 @@
 package android.template.test.app
 
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
